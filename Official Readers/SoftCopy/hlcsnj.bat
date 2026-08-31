9f5c98c4deafd19edf4efe89093dcd5e7dadf72f
Yvan Janssens <yvanj@cyber.gent>
2026-08-31 13:51:37 +0200

    General cleanup pre-release

--- Official Readers/SoftCopy/hlcsnj.bat
+++
@@ -1,4 +0,0 @@
-PUSHD C:\PROGRA~2\IBM\SCR\sys
-SET XERCESC_NLS_HOME=C:\PROGRA~2\IBM\SCR\sys
-java.exe  -cp C:\PROGRA~2\IBM\SCR\sys\XKS.jar;C:\PROGRA~2\IBM\SCR\sys\hlccommon.jar -jar hlcs %1 %2 %3 %4 %5 %6 -d C:\PROGRA~2\IBM\SCR\sys
-POPD
